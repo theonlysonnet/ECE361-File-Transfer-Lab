64dc7e0542225268aff8cc47440f268d0f22aa5e
Salice  Sonnet <saliceso@ug140.eecg.toronto.edu>
2025-01-23 11:45:57 -0500

    made a thing that sends a message based on user input. Need to change it so it checks for existence of a file.

--- deliver.c
+++ deliver.c
@@ -1,6 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main() {
-    printf("Hello, World from deliver!\n");
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server address> <server port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    char *server_address = argv[1];
+    int server_port = atoi(argv[2]);
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in server_addr;
+    socklen_t addr_len = sizeof(server_addr);
+
+    // Create a UDP socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure the server address
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+
+    if (inet_pton(AF_INET, server_address, &server_addr.sin_addr) <= 0) {
+        perror("Invalid server address");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Ask the user for input
+    printf("Enter message: ");
+    fgets(buffer, BUFFER_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = '\0'; // Remove newline character
+
+    // Send the message to the server
+    if (sendto(sockfd, buffer, strlen(buffer), 0, 
+               (const struct sockaddr *)&server_addr, addr_len) < 0) {
+        perror("sendto failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Message sent to server: %s\n", buffer);
+
+    // Receive the server's response
+    memset(buffer, 0, BUFFER_SIZE);
+    int recv_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, 
+                            (struct sockaddr *)&server_addr, &addr_len);
+    if (recv_len < 0) {
+        perror("recvfrom failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    buffer[recv_len] = '\0'; // Null-terminate the received string
+    printf("Server response: %s\n", buffer);
+
+    if (strcmp(buffer, "yes") == 0) {
+        printf("A file transfer can start.\n");
+    }
+
+    close(sockfd);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,6 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main() {
-    printf("Hello, World from server!\n");
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <UDP listen port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int udp_port = atoi(argv[1]);
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+    // Create a UDP socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure the server address
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY; // Listen on all network interfaces
+    server_addr.sin_port = htons(udp_port);
+
+    // Bind the socket to the specified port
+    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d...\n", udp_port);
+
+    // Wait for a message from the client
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int recv_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, 
+                                (struct sockaddr *)&client_addr, &addr_len);
+        if (recv_len < 0) {
+            perror("recvfrom failed");
+            continue;
+        }
+
+        printf("Received message: %s\n", buffer);
+
+        // Respond based on the message
+        const char *response;
+        if (strcmp(buffer, "ftp") == 0) {
+            response = "yes";
+        } else {
+            response = "no";
+        }
+
+        if (sendto(sockfd, response, strlen(response), 0, 
+                   (struct sockaddr *)&client_addr, addr_len) < 0) {
+            perror("sendto failed");
+        } else {
+            printf("Sent response: %s\n", response);
+            break;
+        }
+        break;
+    }
+    close(sockfd);
     return 0;
 }
